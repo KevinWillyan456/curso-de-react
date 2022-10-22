91ba01605457e73c0d373c2c7595166c99f4855d
Kevin Souza <kevinwillyansouza@gmail.com>
2022-10-22 16:53:15 -0400

    Resolvendo erros

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import './MovieList.css'
 import { getMovies } from '../services/api'
 import { MovieCard } from './MovieCard'
+import { Search } from './Search'
 
 export function MovieList(){
     const [movies, setMovies] = useState([])
@@ -13,8 +14,11 @@ export function MovieList(){
         getDataApi()
     },[])
     return (
-        <div className="MovieList">
-            {movies.map(movie=><MovieCard movie={movie}/>)}
-        </div>
+        <>
+        <Search />
+            <div className="MovieList">
+                {movies.map(movie=><MovieCard movie={movie}/>)}
+            </div>
+        </>
     )
 }
