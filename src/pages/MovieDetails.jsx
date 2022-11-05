1b8b22e52924e4f21a834182eb0d473bc86d6535
Kevin Souza <kevin.souza2@estudante.ifms.edu.br>
2022-11-05 16:17:54 -0400

    Modificando o MovieDetails

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,21 @@
+import { useEffect } from "react"
 import { useState } from "react"
 import { useParams } from "react-router-dom"
+import { getMovieInfo } from "../services/api"
 
 export function MovieDetails(){
-    const [movie,setMovie] = useState({})
+    const [movie,setMovie] = useState(null)
     const {id} = useParams('id')
+
+    useEffect(()=>{
+        const getApiData = async() => {
+            setMovie(await getMovieInfo(id))
+        }
+        getApiData()
+    },[])
     return (
         <>
-            <h1>Movie Details {id} </h1>
+            {movie && <img src={movie.images.background} />}
         </>
     )
 }
